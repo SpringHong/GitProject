89b59dff19d42ee9df2d4251b2a0ef05e38bb543
SpringHong <1034537665@qq.com>
2016-07-10 00:20:13 +0800

    Update TestGit.java

--- src/com/atguigu/git/TestGit.java
+++ src/com/atguigu/git/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("hello github!");
 		System.out.println("update 000000");
+		System.out.println("update 111111");
 	}
 }
